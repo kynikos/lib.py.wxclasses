22b30074bcf12d723565c36996c5e619d7d967a3
Dario Giovannetti <dev@dariogiovannetti.net>
2014-11-12 22:29:42 +0800

    Better package structure + setup.py + PKGBUILD

---
+++ src/setup.py
@@ -0,0 +1,26 @@
+# Return a dictionary with the needed variation of a word.
+# Copyright (C) 2011-2014 Dario Giovannetti <dev@dariogiovannetti.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(name='wxclasses',
+      version='1.0.0',
+      description='Classes for wxPython.',
+      author='Dario Giovannetti',
+      author_email='dev@dariogiovannetti.net',
+      url='https://github.com/kynikos/lib.py.wxclasses',
+      license='GPLv3',
+      packages=['wxclasses', ])
